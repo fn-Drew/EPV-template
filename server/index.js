cbf2b9ff49f77f9142c78ae32b190f87290fc5db
fn-Drew <alutter03@gmail.com>
2023-03-25 01:21:27 -0400

    configured for vercel deployment

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const app = require("./app");
 const config = require("./utils/config");
 
-app.listen(config.PORT, () => {
-    console.log("aoirsentarsotien");
-});
-
+if (config.PORT) {
+    app.listen(config.PORT, () => {
+        console.log(`Server is running on port  ${config.PORT}`);
+    });
+}
