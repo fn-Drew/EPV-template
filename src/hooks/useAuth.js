cbf1ed288895a2a6878bd9a68444665b56534af3
fn-Drew <alutter03@gmail.com>
2023-04-18 17:36:23 -0400

    updated login logic to use react query

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-console */
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import loginService from '../services/login';
 import recordService from '../services/records';
 import useCreateUser from './useCreateUser';
+import useLoginUser from './useLoginUser'
 import { setUser } from '../reducers/userReducer';
-import { setCredentials } from '../reducers/credentialsReducer';
 import { setNotification } from '../reducers/notificationReducer';
 
 export default function useAuth(setToggleForm) {
     const credentials = useSelector(state => state.credentials)
     const createUserMutation = useCreateUser();
+    const loginUserMutation = useLoginUser()
     const dispatch = useDispatch();
 
     // on page load check if user has logged in before
@@ -34,19 +34,8 @@ export default function useAuth(setToggleForm) {
 
     const handleLogin = async (event) => {
         event.preventDefault();
-        try {
-            const loggedUser = await loginService.login(credentials);
-            window.localStorage.setItem(
-                "recUserCreds",
-                JSON.stringify(loggedUser)
-            );
-            recordService.setToken(loggedUser.token);
-            dispatch(setUser(loggedUser));
-            dispatch(setCredentials({ username: "", password: "" }));
-            setToggleForm({ accountForm: false, loginForm: false });
-        } catch (err) {
-            console.error(err.response);
-        }
+        loginUserMutation.mutate(credentials);
+        setToggleForm({ accountForm: false, loginForm: false });
     }
 
     const handleLogout = () => {
---
+++ src/hooks/useLoginUser.js
@@ -0,0 +1,45 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useDispatch, useSelector } from "react-redux";
+import { setCredentials } from "../reducers/credentialsReducer";
+import { setNotification } from "../reducers/notificationReducer";
+import { setUser } from "../reducers/userReducer";
+import loginService from "../services/login";
+import recordService from "../services/records"
+
+
+const useLoginUser = () => {
+    const dispatch = useDispatch();
+    const credentials = useSelector(state => state.credentials)
+    return useMutation((userCredentials) => loginService.login(userCredentials), {
+        onSuccess: async () => {
+            const loggedUser = await loginService.login(credentials);
+            window.localStorage.setItem(
+                "recUserCreds",
+                JSON.stringify(loggedUser)
+            );
+            recordService.setToken(loggedUser.token);
+            dispatch(setUser(loggedUser));
+            dispatch(setCredentials({ username: "", password: "" }));
+        },
+        onError: (err) => {
+            dispatch(setNotification(err.response.data.error, 5));
+        }
+    });
+}
+
+// const useLoginUser = (credentials) => {
+//     const dispatch = useDispatch();
+//     useQuery(
+//         ["users"],
+//         () => loginService.login(credentials),
+//         {
+//             refetchOnWindowFocus: false,
+//             retry: false,
+//             onError: (err) => {
+//                 dispatch(setNotification(err.response.data.error, 5));
+//             }
+//         }
+//     )
+// }
+
+export default useLoginUser;
