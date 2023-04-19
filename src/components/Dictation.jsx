a3887d9ebcc6ea5a6c2cd9c8597502126fedabf1
fn-Drew <alutter03@gmail.com>
2023-04-19 17:56:10 -0400

    added icons for header logo & dictation buttons

--- src/components/Dictation.jsx
+++ src/components/Dictation.jsx
@@ -49,7 +49,19 @@ export default function Dictation() {
 
     return (
         <div className="dictation-container">
-            <button className={listening ? "stop-button" : "start-button"} type="button" onClick={toggleListening}>{listening ? 'stop' : 'start'}</button>
+            <button className={listening ? "stop-button" : "start-button"} type="button" onClick={toggleListening}>
+                {listening ?
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="stop-svg">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        <path strokeLinecap="round" fill="red" stroke="red" strokeLinejoin="round" d="M9 9.563C9 9.252 9.252 9 9.563 9h4.874c.311 0 .563.252.563.563v4.874c0 .311-.252.563-.563.563H9.564A.562.562 0 019 14.437V9.564z" />
+                    </svg>
+                    :
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="start-svg">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        <path strokeLinecap="round" fill="lime" stroke="lime" strokeLinejoin="round" d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z" />
+                    </svg>
+                }
+            </button>
             {transcript ? <p>Transcript: {transcript}</p> : <p>&larr; Start transcribing now!</p>}
         </div>
     );
