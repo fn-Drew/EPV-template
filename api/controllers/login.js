7109fdd2f17c07444fc8e823afe9086a1729f8e4
fn-Drew <alutter03@gmail.com>
2023-04-22 16:31:46 -0400

    made user auth tokens last 7 days instead of 1 hour

--- api/controllers/login.js
+++ api/controllers/login.js
@@ -22,7 +22,7 @@ loginRouter.post("/", async (request, response) => {
     };
 
     const token = jwt.sign(userForToken, process.env.SECRET, {
-        expiresIn: 60 * 60,
+        expiresIn: ((60 * 60) * 24) * 7, // 7 days (in seconds)
     });
 
     response.status(200).send({ token, username: user.username, id: user._id });
